d12b7492ee3bcdbacd6bf18d40461c5107981049
Joaovitorlopes-Dev <Joaolopes7193@gmail.com>
2026-05-16 11:31:04 -0300

    Configuração inicial do banco de dados

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class LessonPlan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    objective = db.Column(db.Text)
+    summary = db.Column(db.Text)
+    planned_date = db.Column(db.String(100))
+    discipline = db.Column(db.String(100))
+    contents = db.Column(db.Text)
+    support_resources = db.Column(db.Text)
+    tags = db.Column(db.Text)
+
+    with app.app_context():
+        db.create_all()
 
 @app.route('/')
 def home():
